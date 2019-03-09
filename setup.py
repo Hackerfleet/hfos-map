e8b73c4d2eb1422f31fd8f94478b0f92756be7a0
ri0t <riot@c-base.org>
2019-03-09 18:29:48 +0100

    Fixed packaging, added Trove classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 # HFOS - Hackerfleet Operating System
 # ===================================
-# Copyright (C) 2011-2018 Heiko 'riot' Weinen <riot@c-base.org> and others.
+# Copyright (C) 2011-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -23,40 +23,58 @@ __license__ = "AGPLv3"
 
 from setuptools import setup, find_packages
 
-setup(name="hfos-map",
-      version="0.0.1",
-      description="hfos-map",
+setup(
+    name="hfos-map",
+    version="0.0.1",
+    description="hfos-map",
 
-      author="Hackerfleet Community",
-      author_email="riot@c-base.org",
-      url="https://github.com/hackerfleet/hfos-map",
-      license="GNU Affero General Public License v3",
-      packages=find_packages(),
-      long_description="""HFOS - Map
+    author="Hackerfleet Community",
+    author_email="riot@c-base.org",
+    url="https://github.com/hackerfleet/hfos-map",
+    license="GNU Affero General Public License v3",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Isomer :: 1',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""HFOS - Map
 ==========
 
 A modern, opensource approach to map management.
 
 This software package is a plugin module for HFOS.
 """,
-      dependency_links=[],
-      install_requires=[
-            'isomer>=1.0.0',
-      ],
-      entry_points="""[isomer.components]
-    gdal=hfos.map.gdal:GDAL
-    mts=hfos.map.maptileservice:MaptileService
-    mtl=hfos.map.maptileservice:MaptileLoader
+    dependency_links=[],
+    install_requires=[
+        'isomer>=1.0.0',
+    ],
+    entry_points="""[isomer.components]
+    gdal=isomer.map.gdal:GDAL
+    mts=isomer.map.maptileservice:MaptileService
+    mtl=isomer.map.maptileservice:MaptileLoader
 [isomer.schemata]
-    geoobject=hfos.map.schemata.geoobject:GeoObject
-    layer=hfos.map.schemata.layer:Layer
-    layergroup=hfos.map.schemata.layergroup:LayerGroup
-    mapview=hfos.map.schemata.mapview:MapView
-    route=hfos.map.schemata.route:Route
+    geoobject=isomer.map.schemata.geoobject:GeoObject
+    layer=isomer.map.schemata.layer:Layer
+    layergroup=isomer.map.schemata.layergroup:LayerGroup
+    mapview=isomer.map.schemata.mapview:MapView
+    route=isomer.map.schemata.route:Route
 [isomer.provisions]
-    layer=hfos.map.provisions.layer:provision
-    layergroup=hfos.map.provisions.layergroup:provision
-    mapview=hfos.map.provisions.mapview:provision
+    layer=isomer.map.provisions.layer:provision
+    layergroup=isomer.map.provisions.layergroup:provision
+    mapview=isomer.map.provisions.mapview:provision
     """,
-      test_suite="tests.main.main",
-      )
+    test_suite="tests.main.main",
+)
